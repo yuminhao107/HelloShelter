c23f13a2eebb1915149b578a4e2d25d70565312a
yuminhao107 <yuminhao107@gmail.com>
2018-08-08 16:59:32 +0800

    add PetContract.java

---
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -0,0 +1,16 @@
+package com.example.android.pets.data;
+
+public final class PetContract {
+
+    public static final String TABLE_NAME = "pets";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_NAME = "name";
+    public static final String COLUMN_BREED = "breed";
+    public static final String COLUMN_GENDAR = "gendar";
+    public static final String COLUMN_WEIGHT = "weight";
+
+    public static final int GENDAR_UNKNOW = 0;
+    public static final int GENDAR_MALE = 1;
+    public static final int GENDAR_FEMALE = 2;
+
+}
