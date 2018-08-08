9ba8df132fe9e1a90148e60d2b3e49f560ede263
yuminhao107 <yuminhao107@gmail.com>
2018-08-08 17:26:44 +0800

    edit PetContract.java

--- app/src/main/java/com/example/android/pets/data/PetContract.java
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -3,14 +3,13 @@ package com.example.android.pets.data;
 public final class PetContract {
 
     public static final String TABLE_NAME = "pets";
-    public static final String COLUMN_ID = "_id";
-    public static final String COLUMN_NAME = "name";
-    public static final String COLUMN_BREED = "breed";
-    public static final String COLUMN_GENDAR = "gendar";
-    public static final String COLUMN_WEIGHT = "weight";
+    public static final String _ID = "_id";
+    public static final String COLUMN_PET_NAME = "name";
+    public static final String COLUMN_PET_BREED = "breed";
+    public static final String COLUMN_PET_GENDAR = "gendar";
+    public static final String COLUMN_PET_WEIGHT = "weight";
 
-    public static final int GENDAR_UNKNOW = 0;
+    public static final int GENDAR_UNKNOWN = 0;
     public static final int GENDAR_MALE = 1;
     public static final int GENDAR_FEMALE = 2;
-
 }
