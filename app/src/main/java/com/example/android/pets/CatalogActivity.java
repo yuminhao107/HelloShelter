eaf8529a7dcff62b6eba4bdb84ebe0be2b29b822
yuminhao107 <yuminhao107@gmail.com>
2018-08-10 19:31:39 +0800

    modify CatalogActivity.java

--- app/src/main/java/com/example/android/pets/CatalogActivity.java
+++ app/src/main/java/com/example/android/pets/CatalogActivity.java
@@ -15,12 +15,14 @@
  */
 package com.example.android.pets;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -28,6 +30,8 @@ import android.widget.TextView;
 
 import com.example.android.pets.data.PetContract;
 import com.example.android.pets.data.PetDbHelper;
+import com.example.android.pets.data.PetContract.PetEntry;
+
 
 /**
  * Displays list of pets that were entered and stored in the app.
@@ -76,6 +80,17 @@ public class CatalogActivity extends AppCompatActivity {
         }
     }
 
+    private void insertDummyData() {
+        PetDbHelper petDbHelper = new PetDbHelper(this);
+        SQLiteDatabase db = petDbHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(PetEntry.COLUMN_PET_NAME, "Toto");
+        values.put(PetEntry.COLUMN_PET_BREED, "Terriar");
+        values.put(PetEntry.COLUMN_PET_GENDER, PetEntry.GENDAR_MALE);
+        values.put(PetEntry.COLUMN_PET_WEIGHT, 14);
+        Long newRowID = db.insert(PetEntry.TABLE_NAME, null, values);
+        Log.v("CatalogActivity", "Rowid is: " + newRowID);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu options from the res/menu/menu_catalog.xml file.
@@ -90,7 +105,8 @@ public class CatalogActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             // Respond to a click on the "Insert dummy data" menu option
             case R.id.action_insert_dummy_data:
-                // Do nothing for now
+                insertDummyData();
+                displayDatabaseInfo();
                 return true;
             // Respond to a click on the "Delete all entries" menu option
             case R.id.action_delete_all_entries:
